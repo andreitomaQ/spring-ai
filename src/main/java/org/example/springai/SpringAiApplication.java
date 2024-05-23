0b57b7f4747478fe4cd49ac2b68c8c11aa7b6132
Toma Andrei <andrei.toma@qualitance.com>
2024-05-23 10:26:42 +0300

    spring data jpa reference documentation assistant

--- src/main/java/org/example/springai/SpringAiApplication.java
+++ src/main/java/org/example/springai/SpringAiApplication.java
@@ -2,7 +2,11 @@ package org.example.springai;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportRuntimeHints;
+import org.springframework.shell.command.annotation.CommandScan;
 
+@CommandScan
+@ImportRuntimeHints(HintsRegistrar.class)
 @SpringBootApplication
 public class SpringAiApplication {
 
